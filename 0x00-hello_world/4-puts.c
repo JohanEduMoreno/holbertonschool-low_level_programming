6f1faa1467f19066bba51088f79e2cd4e17c4396
JohanEduMoreno <you@example.com>
2022-02-03 16:14:38 -0800

    first hello word in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main (void){
+
+/**
+ * hello word
+ *
+ *hello word  
+ */
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
