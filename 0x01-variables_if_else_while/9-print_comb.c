9da228630dbf670368dcc0e9b58f12deda3782eb
JohanEduMoreno <you@example.com>
2022-02-07 13:04:15 -0800

    all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+	putchar(x);
+	if (x != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	x++;
+	}
+
+	putchar(10);
+	return (0);
+}
