b7a39e6652a62e6420768049b658982a008a13bc
JohanEduMoreno <you@example.com>
2022-02-07 09:20:56 -0800

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; --n)
+	putchar(n);
+
+	putchar(10);
+	return (0);
+}
