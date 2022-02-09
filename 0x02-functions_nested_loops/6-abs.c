975dedcbab263e054c43866d6019ed7312bc64b9
JohanEduMoreno <you@example.com>
2022-02-08 20:53:36 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+	return (n);
+	}
+	else if (n == 0)
+	{
+	return (0);
+	}
+	else
+	{
+	return (-n);
+	}
+}
